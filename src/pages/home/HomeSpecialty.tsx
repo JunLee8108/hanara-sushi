7519a578d5747d6bc2545941a4ceee5373717a2a
JunLee8108 <lejhn1@gmail.com>
2023-11-09 00:52:28 -0600

    quick fix for the carousel

--- src/pages/home/HomeSpecialty.tsx
+++ src/pages/home/HomeSpecialty.tsx
@@ -7,8 +7,6 @@ const HallelujahRoll = require("../../assets/Hallelujah-Roll.webp");
 
 export default function HomeSpecialty() {
   const [currentImage, setCurrentImage] = useState(0);
-  const [isDragging, setIsDragging] = useState(false);
-  const [dragStartX, setDragStartX] = useState(0);
 
   const specialtyData = [
     {
@@ -35,42 +33,13 @@ export default function HomeSpecialty() {
     );
   };
 
-  const handleTouchStart = (event: React.TouchEvent) => {
-    const touchX = event.touches[0].clientX;
-    setDragStartX(touchX);
-  };
-
-  const handleTouchMove = (event: React.TouchEvent) => {
-    if (!isDragging) {
-      setIsDragging(true);
-    }
-    const touchX = event.touches[0].clientX;
-    const diffX = touchX - dragStartX;
-
-    if (diffX > 150) {
-      // Threshold to consider as a drag, adjust as needed
-      goToPrevious();
-      setIsDragging(false);
-    } else if (diffX < -150) {
-      goToNext();
-      setIsDragging(false);
-    }
-  };
-
-  const handleTouchEnd = () => {
-    setIsDragging(false);
-  };
-
   return (
     <div className="home-specialty-container home-common-container mg-left-right-auto">
-      <h1 className="home-title-border-left">Our Specialty Rolls</h1>
+      <h1 className="home-specialty-title home-title-border-left mg-left-right-auto">
+        Our Specialty Rolls
+      </h1>
 
-      <div
-        className="home-specialty-img-container mg-left-right-auto"
-        onTouchStart={handleTouchStart}
-        onTouchMove={handleTouchMove}
-        onTouchEnd={handleTouchEnd}
-      >
+      <div className="home-specialty-img-container mg-left-right-auto">
         {specialtyData.map((src, index) => {
           return (
             <img
@@ -81,7 +50,6 @@ export default function HomeSpecialty() {
               className={`home-specailty-img ${
                 index === currentImage ? "active" : ""
               }`}
-              draggable="false"
             />
           );
         })}
