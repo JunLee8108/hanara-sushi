27659191ab10d72132b6b8582df5663f9faa99a8
JunLee8108 <lejhn1@gmail.com>
2023-11-14 01:17:28 -0600

    icon added and our specialty rolls section added

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,12 +3,14 @@ import HomeIntro from "./HomeIntro";
 import HomeLocation from "./HomeLocation";
 import HomePromotion from "./HomePromotion";
 import HomeContact from "./HomeContact";
+import MenuSpecialty from "../menu/MenuSpecialty";
 
 export default function Home() {
   return (
     <div className="home-container mg-left-right-auto">
       <HomeIntro />
       <HomeLocation />
+      <MenuSpecialty />
       <HomePromotion />
       <HomeContact />
     </div>
